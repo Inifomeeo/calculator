17a7d2d2316b035174ea0f62643500b8773b1eac
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-04-15 11:39:52 +0100

    Add function that toggles between operators

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ let subtract = (arg1, arg2) => arg1 - arg2;
 let multiply = (arg1, arg2) => arg1 * arg2;
 
 let divide = (arg1, arg2) => arg1 / arg2;
+
+let firstNum;
+let operator;
+let secondNum;
+
+let operate = (operatorArg, numArg1, numArg2) => {
+    switch (operatorArg) {
+        case "+":
+            return(add(numArg1,numArg2));
+            break;
+        case "-":
+            return(subtract(numArg1, numArg2));
+            break;
+        case "*":
+            return(multiply(numArg1, numArg2));
+            break;
+        case "/":
+            return(divide(numArg1, numArg2));
+            break;
+        default:
+            alert("Unidentified operator");
+            break;
+    }
+};
