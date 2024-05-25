bfe6708a1b9725dd9306db40d5132f46fe524aba
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-05-25 19:06:57 +0100

    Add clearDisplay function

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let add = (arg1, arg2) => arg1 + arg2;
-
 let subtract = (arg1, arg2) => arg1 - arg2;
-
 let multiply = (arg1, arg2) => arg1 * arg2;
-
 let divide = (arg1, arg2) => arg1 / arg2;
 
+let displayValue;
 let firstOperand;
 let operator;
 let secondOperand;
+let result;
+
+const numButtons = document.querySelectorAll('.operand');
+const operatorButtons = document.querySelectorAll('.operator');
+const allClearButton = document.querySelector('.clear');
+const signButton = document.querySelector('.sign');
+const percentButton = document.querySelector('.percentage');
+const equalsButton = document.querySelector('.equals');
+
+let clearDisplay = () => {
+    displayValue = '0';
+    firstOperand = null;
+    secondOperand = null;
+    operator = null;
+    result = null;
+}
 
 let operate = (operatorArg, firstNum, secondNum) => {
     switch (operatorArg) {
